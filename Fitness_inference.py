5fcf2a0c468de1028356c395783efcaa11927c41
Jack Edwards <jedwards1@fas.harvard.edu>
2023-01-26 12:34:31 -0500

    Updated Fitness pipeline .py file. Testing for consistency

--- Fitness_inference.py
+++ Fitness_inference.py
@@ -1,5 +1,4 @@
 # Imports
-import sys
 import numpy as np
 import pymc3 as pm
 import pandas as pd
@@ -11,9 +10,39 @@ import fitness_mcmc.data_io as io
 import fitness_mcmc.fitness_mcmc as m
 import fitness_mcmc.format_raw as raw
 
-# Input parameters
-population = sys.argv[1]
-environment = sys.argv[2]
-replicates = sys.argv[3]
+
+def fitness_pipeline(population, environment, output, replicates=3, format=True):
+    """
+    Serves as the master pipeline for getting a single environment's (and replicates) inferred fitness values. Will
+    update these comments as I develop functionality here.
+
+    Parameters:
+        population(str) - LTEE population of interest
+        environment(str) - environment of fitness assay
+        output(str) - tag for output files
+        replicates(int) - # of replicates to be processed
+        format(bool) - whether or not to format from raw barcode counts
+    """
+    if format:
+        raw.format_data(population, environment, replicates)
+
+    # Loop for all replicates
+    for rep in range(1, replicates + 1):
+        # Loading data with data_io
+        r = population + "_" + environment + str(rep)
+        data, time, ordered_counts = io.load_data("LTEE_" + r + ".csv", return_ordered=False, delimiter=",")
+
+        # Creating fitness model and getting inferences with fitness_mcmc
+        fitness_model = m.Fitness_Model(ordered_counts, time, s_ref=1, prior="flat")
+        fitness_model.find_MAP()
+
+        # Retrieving "s" and "f0" values from the model
+        raw_s = fitness_model.map_estimate["s"]
+        vals_f0 = fitness_model.map_estimate["f0"]
+        vals_s = np.zeros((29))
+        vals_s[0] = 0
+        for i in range(0, len(raw_s)):
+            vals_s[i + 1] = raw_s[i]
+        vals_data = fitness_model.data
 
 

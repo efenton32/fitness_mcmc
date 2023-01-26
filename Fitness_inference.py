47aa0f3fb216f81bcf665db32e04d5d88d96b21b
Jack Edwards <jedwards1@fas.harvard.edu>
2023-01-26 11:12:50 -0500

    Added all m5_150 bc count files to the raw data folder. Testing format

--- Fitness_inference.py
+++ Fitness_inference.py
@@ -1,3 +1,4 @@
+# Imports
 import sys
 import numpy as np
 import pymc3 as pm
@@ -8,6 +9,11 @@ import arviz as az
 import fitness_mcmc
 import fitness_mcmc.data_io as io
 import fitness_mcmc.fitness_mcmc as m
-import fitness_mcmc.format_raw as format
+import fitness_mcmc.format_raw as raw
+
+# Input parameters
+population = sys.argv[1]
+environment = sys.argv[2]
+replicates = sys.argv[3]
 
 
--- fitness_mcmc/format_raw.py
+++ fitness_mcmc/format_raw.py
@@ -1,9 +1,40 @@
+import os
 import csv
 import numpy as np
 import pandas as pd
 
 
+def get_dir(file_name, sub_dir):
+    """
+    This function will return the correct path to the relevant data files for the LTEE data.
+    This file is designed to work with the processed barcode counts from LTEE fitness assays. For other users,
+    please format the data as seen in the regular documentation.
+
+    Parameters:
+        file_name(str) - name of the data file including file type
+        sub_dir(str) - name of the folder in which the desired file resides
+
+    Returns:
+        data_path(str) - the exact path to where the named file resides
+    """
+    start = os.path.abspath(__file__)
+    start_dir = os.path.dirname(start)
+    data_path = os.path.join(start_dir, sub_dir, file_name)
+
+    return data_path
+
+
 def format_data(population, environment, replicates):
+    """
+    Pipeline for formatting daily fitness assay barcode counts into a single file. Currently only works when provided a
+    master file with barcode associations and some relevant naming scheme for timepoints. This will work with the first
+    batch of fitness assays from Tanush, but will need to be updated for robustness in the future. For now, we ball.
+
+    Parameters:
+        population(str) - the LTEE population, may be removed in the future
+        environment(str) - growth environment of the assay
+        replicates(int) - the number of total replicates to format
+    """
     pop = population
     env = environment
 
@@ -26,7 +57,8 @@ def format_data(population, environment, replicates):
             gens[timepoints[i]] = i + 19
 
         barcodes = {}                   # dict of barcode sequence: generation value
-        with open("m5d0_j.csv", 'rt') as f:
+        bc_file = get_dir("m5d0_j.csv", "raw_data")
+        with open(bc_file, 'rt') as f:
             reader = csv.reader(f)
             labels = next(reader)
             for i in range(1, 30):
@@ -38,7 +70,8 @@ def format_data(population, environment, replicates):
 
         for r in range(1, 6):
             list_file = pop + "_" + env + "_" + rep + "_" + str(r) + ".csv"
-            first_read = pd.read_csv(list_file, sep=",", header=0)
+            data_file = get_dir(list_file, "raw_data")
+            first_read = pd.read_csv(data_file, sep=",", header=0)
             lim = len(first_read.index)
             with open(list_file, "rt") as g:
                 reader = csv.reader(g)
@@ -50,7 +83,8 @@ def format_data(population, environment, replicates):
                     frame[index][r + 1] = int(data[1])
             g.close()
 
-        out = open("LTEE_" + pop + "_" + env + rep + ".csv", "w")
+        file_out = get_dir("LTEE_" + pop + "_" + env + rep + ".csv", "experimental_data")
+        out = open(file_out, "w")
         holder = header[0]
         for w in range(1, len(header)):
             holder += "," + header[w]
@@ -64,3 +98,7 @@ def format_data(population, environment, replicates):
             out.write("\n")
 
         out.close()
+        print("formatting done")
+
+
+format_data("pop1", "gluL", 3)

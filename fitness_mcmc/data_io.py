db28ef3370b557cd005ea71f3aab1630bafbd3e0
Jack Edwards <jedwards1@fas.harvard.edu>
2023-01-26 12:32:45 -0500

    Revert "Finalized multiple replicates"
    This reverts commit 9f88ea1395d9eedbf70a4a33afa5ba51b16df7ef.

--- fitness_mcmc/data_io.py
+++ fitness_mcmc/data_io.py
@@ -45,11 +45,11 @@ def load_data(filename, data_dir = "experimental_data", load_metadata = False,
     """
     data_file = _get_file_path(filename, data_dir)
     if return_times:
-        data = pd.read_csv(data_file, delimiter = delimiter, index_col = 0)
+        data = pd.read_csv(data_file, delimiter = delimiter)
         time = [float(i) for i in data.columns[1:]]
         counts = data.loc[:,data.columns[1:]].to_numpy()
     else:
-        data = pd.read_csv(data_file, delimiter = delimiter, header = None, index_col = 0)
+        data = pd.read_csv(data_file, delimiter = delimiter, header = None)
         counts = data.loc[:,data.columns[1:]].to_numpy()
 
     if load_metadata:
@@ -62,9 +62,8 @@ def load_data(filename, data_dir = "experimental_data", load_metadata = False,
 
     if return_ordered:
         idx = np.flipud(np.argsort(np.sum(counts, axis = 1)))
-        bc_idx = data.index.to_numpy()[idx]
         counts = counts[idx, :].astype("float")
-        data = data.reindex(bc_idx)
+        data = data.reindex(idx)
 
     counts = counts.reshape([counts.shape[0], counts.shape[1], 1])
 
